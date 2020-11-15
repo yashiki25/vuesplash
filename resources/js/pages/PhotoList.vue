1fec455c21c2f1664f3fb9951c6c1ddfefe1bccd
yashiki <ymk.gr.22@gmail.com>
2020-11-16 03:29:48 +0900

    Add 写真一覧・ログイン画面のルーティングを設定

---
+++ resources/js/pages/PhotoList.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Photo List</h1>
+</template>
+
+<script>
+export default {
+name: "PhotoList"
+}
+</script>
---
+++ resources/js/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import VueRouter from "vue-router";
+
+import PhotoList from "./pages/PhotoList";
+import Login from "./pages/Login";
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login,
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
+
