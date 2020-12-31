4937edd7c0200be8c86d608d75076bc87925bc95
yashiki <ymk.gr.22@gmail.com>
2020-12-31 21:57:01 +0900

    Add ヘッダー・フッターを追加

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
+    <header>
+      <Navbar />
+    </header>
     <main>
       <div class="container">
         <RouterView />
       </div>
     </main>
+    <footer>
+      <Footer />
+    </footer>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 export default {
-name: "App.vue"
+name: "App.vue",
+  components: {
+    Navbar,
+    Footer
+  }
 }
 </script>
 
---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <button class="button button--link">Logout</button>
+    <RouterLink class="button button--link" to="/login">
+      Login / Register
+    </RouterLink>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
