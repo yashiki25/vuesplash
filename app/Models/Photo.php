8142dce9c981d3acc2e338215743b08f47fba692
yashiki <ymk.gr.22@gmail.com>
2021-01-01 17:49:03 +0900

    Add Photoインスタンス生成時にランダムIDを生成する

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Photo extends Model
 {
     protected $table = 'photos';
+
+    protected $keyType = 'string';
+
+    /** IDの桁数 */
+    const ID_LENGTH = 12;
 
     protected $guarded = [
         'created_at',
@@ -17,5 +23,44 @@ class Photo extends Model
         'updated_at',
         'created_at',
     ];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        if (! Arr::get($this->attributes, 'id')) {
+            $this->setId();
+        }
+    }
+
+    /**
+     * ランダムなID値をid属性に代入する
+     */
+    private function setId()
+    {
+        $this->attributes['id'] = $this->getRandomId();
+    }
+
+    /**
+     * ランダムなID値を生成する
+     * @return string
+     * @throws \Exception
+     */
+    private function getRandomId()
+    {
+        $characters = array_merge(
+            range(0, 9), range('a', 'z'),
+            range('A', 'Z'), ['-', '_']
+        );
+
+        $length = count($characters);
+
+        $id = '';
+        for ($i = 0; $i < self::ID_LENGTH; $i++) {
+            $id .= $characters[random_int(0, $length - 1)];
+        }
+
+        return $id;
+    }
 
 }
